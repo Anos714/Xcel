361d3abd9dd8d99650da1db8bfd8ec674a701d0b
Rahul Sain <sainrahul374@gmail.com>
2026-07-17 08:28:29 +0530

    feat: setup axios client

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,19 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
 });
+
+// response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error(error);
+
+    return Promise.reject(error);
+  },
+);
